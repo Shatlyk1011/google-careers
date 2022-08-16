67ff2815c14183bb819d8f5a443c7c6fe9138814
Shatlyk <shatlykabdullayev@main.ru>
2022-08-16 15:04:06 +0300

    added watch and coverage stages to unit test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-    <h1 class="mt-5">Hello World!</h1>
+    <h1 class="mt-8 text-brand-green-1">Hello World!</h1>
+    <MainNav />
 </template>
 
 <script>
+import MainNav from '@/components/MainNav.vue';
 export default {
-    name: "App",
-    components: {},
+    name: 'App',
+    components: { MainNav },
 };
 </script>
